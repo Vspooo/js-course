d5f64e10db74565bd4bcb8a9c6ea0dcd63f1f1d3
Vspooo <voloshunns@gmail.com>
2024-03-14 18:49:51 +0200

    completed hm9, hm5,corrected add8

--- hm/hm8/additional/add8.js
+++ hm/hm8/additional/add8.js
@@ -1,6 +1,8 @@
 // написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив. масив вивести в консоль
 let elementsWithClass = document.querySelectorAll("[class]")
+let array = []
 for (let i = 0; i < elementsWithClass.length; i++) {
     const elementWithClass = elementsWithClass[i];
-    console.log(elementWithClass.className.split(" "));
+    array.push(elementWithClass.className)
 }
+console.log(array)
